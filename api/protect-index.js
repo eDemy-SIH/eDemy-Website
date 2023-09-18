7cb74666de4290b9d13d2e463159b391e7859880
Satyaki Dey <sdey2836@gmail.com>
2023-09-19 01:19:41 +0530

    urghh

--- api/protect-index.js
+++ api/protect-index.js
@@ -1,14 +1,44 @@
 module.exports = (req, res) => {
   const userAgent = req.headers['user-agent'];
-  console.log(headers);
-  // Check if the request is coming from a web browser
-  if (userAgent.includes('Mozilla')) {
-    // If the user agent contains "Mozilla," it's likely a web browser
-    // You can redirect them to another page or return an error message
-    res.writeHead(302, { Location: '/' });
-    res.end();
-  } else {
-    // For other user agents (like HTTrack), you can return an error message or take appropriate action
+
+  const blockedUserAgents = [
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)',
+    'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0)',
+    'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)',
+    'Mozilla/4.0 (compatible; MSIE 5.0; Win32)',
+    'Mozilla/4.5 (compatible; MSIE 4.01; Windows 98)',
+    'Mozilla/4.5 (compatible; MSIE 4.01; Windows 95)',
+    'Mozilla/4.5 (compatible; MSIE 4.01; Windows NT)',
+    'Mozilla/4.78 [en] (Windows NT 5.0; U)',
+    'Mozilla/4.61 [en] (Win98; I)',
+    'Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.1) Gecko/20020826',
+    'Mozilla/3.0 (Win95; I)',
+    'Mozilla/2.0 (compatible; MSIE 3.01; Windows 95)',
+    'Mozilla/4.5 [en] (X11; I; Linux 2.0.34 i686)',
+    'Mozilla/4.06C-EMS-1.4 [en] (X11; U; SunOS 5.5.1 sun4m)',
+    'Mozilla/4.5 (Macintosh; I; PPC)',
+    'Mozilla/3.01-C-MACOS8 (Macintosh; I; PPC)',
+    'Mozilla/2.0 (OS/2; I)',
+    'Mozilla/4.5 [en] (X11; U; SunOS 5.6 sun4u)',
+    'Mozilla/4.5 [en] (X11; I; AIX 4.1)',
+    'Mozilla/4.5 [en] (X11; I; FreeBSD 2.2.6-RELEASE i386)',
+    'Mozilla/3.01SGoldC-SGI (X11; I; IRIX 6.3 IP32)',
+    'Mozilla/3.0 WebTV/1.2 (compatible; MSIE 2.0)',
+    'Mozilla/2.0 (compatible; MS FrontPage Express 2.0)',
+    'Mozilla/4.05 [fr] (Win98; I)',
+    'Lynx/2.8rel.3 libwww-FM/2.14',
+    'Java1.1.4',
+    'Mozilla/4.5 (compatible; HTTrack 3.0x; Windows 98)',
+    'HyperBrowser (Cray; I; OrganicOS 9.7.42beta-27)',
+    'HTTrack/3.0x',
+    'HTTrack Website Copier/3.0x (offline browser; web mirror utility)'
+  ];
+
+  if (blockedUserAgents.some(agent => userAgent.includes(agent))) {
+    // For HTTrack user agents, return a "403 Forbidden" status
     res.status(403).send('Access Denied');
+  } else {
+    // Allow the request to continue for other user agents
+    res.end();
   }
 };
