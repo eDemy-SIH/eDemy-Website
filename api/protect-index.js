eba7c1a35a0932119e279275744483d1488b0303
Satyaki Dey <sdey2836@gmail.com>
2023-09-19 00:46:52 +0530

    s

--- api/protect-index.js
+++ api/protect-index.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
   if (userAgent.includes('Mozilla')) {
     // If the user agent contains "Mozilla," it's likely a web browser
     // You can redirect them to another page or return an error message
-    res.writeHead(302, { Location: '/error.html' });
+    res.writeHead(302, { Location: '/index.html' });
     res.end();
   } else {
     // For other user agents (like HTTrack), you can return an error message or take appropriate action
